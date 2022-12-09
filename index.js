cd0ad63890a9e869d2f0eeddc9ba32efdc084ab6
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-12-09 18:38:10 +0100

    import sound

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-
+// import a victory sound file
 let trueNum = Math.floor(Math.random() * 1000) + 1
 let resultContainer = document.querySelector('.result-container')
 let answer = false
+
 
 // console.log(trueNum)
 
 function inputValidation (input) {
     if (input === trueNum) {
+        let victory = new Audio('victory.mp3')
+        victory.volume = 0.5
+        victory.play()
         console.log('You guessed the number!')
         const win = document.createElement('p')
-        win.textContent = 'You guessed the number! NEW NUMBER GENERATED'
+        win.textContent = "GG c'était bien " + trueNum + " ! Nouveau nombre généré"
         resultContainer.innerHTML = ''
         win.style.fontSize = '1.5rem'
         win.style.color = 'green'
         win.style.fontWeight = 'bold'
+        // text align center
+        win.style.textAlign = 'center'
         resultContainer.appendChild(win)
+        
         answer = true
     } else if (input > trueNum) {
         console.log('Too high!')
         const high = document.createElement('p')
-        high.textContent = 'Too high!'
+        high.textContent = 'Trop haut!'
         resultContainer.innerHTML = ''
         high.style.fontSize = '1.5rem'
         high.style.color = 'red'
         high.style.fontWeight = 'bold'
+        high.style.textAlign = 'center'
         resultContainer.appendChild(high)
     }
     else if (input < trueNum) {
         console.log('Too low!')
         const low = document.createElement('p')
-        low.textContent = 'Too low!'
+        low.textContent = 'Trop bas!'
         resultContainer.innerHTML = ''
         low.style.fontSize = '1.5rem'
-        low.style.color = 'blue'
+        low.style.color = 'lightblue'
         low.style.fontWeight = 'bold'
+        low.style.textAlign = 'center'
         resultContainer.appendChild(low)
     }
     else if (isNaN(input)) {
         console.log('Please enter a number!')
         const notNum = document.createElement('p')
-        notNum.textContent = 'Please enter a number!'
+        notNum.textContent = 'Ce n\'est pas un nombre!'
         resultContainer.innerHTML = ''
         notNum.style.color = '#' + randomColor
         resultContainer.appendChild(notNum)
